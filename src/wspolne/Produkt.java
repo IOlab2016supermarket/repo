6f69b43a7838d2143f30e38de111b136475d430c
IOlab2016supermarket <iolab2016supermarket@gmail.com>
2016-12-07 14:50:59 +0800

    Dodane metody i pola

--- src/wspolne/Produkt.java
+++ src/wspolne/Produkt.java
@@ -12,18 +12,137 @@ import java.sql.Date;
  * @author Wojtass
  */
 public class Produkt {
-    
-    private int idProdukt;
-    private float waga;
-    private String nazwa;
+	
+	private int id_produktu;
+	private String nazwa;
     private int dlugoscGwarancji;
-    private Date dataWaznosci;
-    private float cenaZakupu;
-    private float cenaSprzedazy;
-    private float cenaPromocyjna;
-    private int ilosc;
-    private int nr_regalu;
-    private int nr_polki;
-    private int nr_miejsca;
-    private int punkty;
+    private double cenaZakupu;
+	private double cenaSprzedazy;
+	private double cenaPromocyjna;
+	private double waga;
+	private int ilosc;
+	//private Date dataWaznosci;
+    //private int nr_regalu;
+    //private int nr_polki;
+    //private int nr_miejsca;
+    //private int punkty;
+	
+	public Produkt(int id_produktu,String nazwa,int dlugoscGwarancji,double cenaZakupu,double cenaSprzedazy,double cenaPromocyjna,double waga,int ilosc)
+	{
+		this.id_produktu = id_produktu;
+		this.nazwa = nazwa;
+		this.dlugoscGwarancji = dlugoscGwarancji;
+		this.cenaZakupu = cenaZakupu;
+		this.cenaSprzedazy = cenaSprzedazy;
+		this.cenaPromocyjna = cenaPromocyjna;
+		this.waga = waga;
+		this.ilosc = ilosc;
+	}
+	
+	public Produkt(Produkt produkt)
+	{
+		id_produktu = produkt.pobierzId();
+		nazwa = produkt.pobierzNazwe();
+		dlugoscGwarancji = produkt.pobierzDlugoscGwarancji();
+		cenaZakupu = produkt.pobierzCeneZakupu();
+		cenaSprzedazy = produkt.pobierzCeneSprzedazy();
+		cenaPromocyjna = produkt.pobierzCenePromocyjna();
+		waga = produkt.pobierzWage();
+		ilosc = produkt.pobierzIlosc();
+	}
+	
+	public Produkt()
+	{
+		
+	}
+	
+	public void ustawDlugoscGwarancji(int dlugoscGwarancji)
+	{
+		this.dlugoscGwarancji = dlugoscGwarancji;
+	}
+	
+	public int pobierzDlugoscGwarancji()
+	{
+		return dlugoscGwarancji;
+	}
+	
+	public void ustawId(int id_produktu)
+	{
+		this.id_produktu = id_produktu;
+	}
+	
+	public int pobierzId()
+	{
+		return id_produktu;
+	}
+	
+	public int pobierzIlosc()
+	{
+		return ilosc;
+	}
+	
+	public void zwiekszIlosc(int ilosc)
+	{
+		this.ilosc += ilosc;
+	}
+	
+	public void ustawIlosc(int ilosc)
+	{
+		this.ilosc = ilosc;
+	}
+	
+	public void ustawCeneZakupu(double cenaZakupu)
+	{
+		this.cenaZakupu = cenaZakupu;
+	}
+	
+	public double pobierzCeneZakupu()
+	{
+		return cenaZakupu;
+	}
+	
+	public void ustawCeneSprzedazy(double cenaSprzedazy)
+	{
+		this.cenaSprzedazy = cenaSprzedazy;
+	}
+	
+	public double pobierzCeneSprzedazy()
+	{
+		return cenaSprzedazy;
+	}
+	
+	public void ustawCenePromocyjna(double cenaPromocyjna)
+	{
+		this.cenaPromocyjna = cenaPromocyjna;
+	}
+	
+	public double pobierzCenePromocyjna()
+	{
+		return cenaPromocyjna;
+	}
+	
+	public void ustawWage(double waga)
+	{
+		this.waga = waga;
+	}
+	
+	public double pobierzWage()
+	{
+		return waga;
+	}
+	
+	public void ustawNazwe(String nazwa)
+	{
+		this.nazwa = nazwa;
+	}
+	
+	public String pobierzNazwe()
+	{
+		return nazwa;
+	}
+	
+	public String toString()
+	{
+		return nazwa+" cena: "+cenaSprzedazy;
+	}
 }
--- src/wspolne/Zamowienie.java
+++ src/wspolne/Zamowienie.java
@@ -5,28 +5,112 @@
  */
 package wspolne;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 /**
  *
  * @author Wojtass
  */
 public class Zamowienie {
+
+	private int nr_zamowienia;
+	private List<Produkt> produkty;
+	private String status;
+	private int czas_dostawy;
+	private Date data_zlozenia; 
+	private Date data_dostawy;
+	private int id_sprzedawcy;
+    //private int idProdukt;
+    //private int iloscProduktow;
     
-    private List<Produkt> produkty =null;
-    private int idZamowienie;
-    private  int idProudkt;
-    private int idSprzedawca;
-    private int iloscProduktow;
-    private String status;
-    private double czasDostawy;
-  //  private Dostawca dostawca;  dopoki nie importuje dostawy to komentuje.    
-    private int nrZamowienia;
-    
-    public Zamowienie(){
-        produkty = new ArrayList<>();
-    }
-    
-    
+	public Zamowienie(int nr_zamowienia,List<Produkt> produkty,int czas_dostawy,String data_zlozenia, int id_sprzedawcy)
+	{
+		//String data_zlozenia w formacie dd/mm/yyyy
+		this.nr_zamowienia = nr_zamowienia;
+		this.produkty = new ArrayList<Produkt>();
+		for(int i = 0; i < produkty.size(); i++)
+		{
+			this.produkty.add(new Produkt(produkty.get(i)));
+		}
+		this.czas_dostawy = czas_dostawy;
+		SimpleDateFormat simple = new SimpleDateFormat("dd/MM/yyyy");
+		try 
+		{
+			this.data_zlozenia = simple.parse(data_zlozenia);
+		} 
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
+		this.id_sprzedawcy = id_sprzedawcy;
+		status = new String("W trakcie realizacji");
+	}
+
+	public int pobierzNrZamowienia()
+	{
+		return nr_zamowienia;
+	}
+	
+	public String sprawdzStatus()
+	{
+		return status;
+	}
+	
+	public double zliczWartoscZamowienia()
+	{
+		double wartosc = 0;
+		for(int i = 0; i < produkty.size(); i++)
+		{
+			wartosc += produkty.get(i).pobierzCeneSprzedazy();
+		}
+		return wartosc;
+	}
+	
+	public int pobierzIdSprzedawcy()
+	{
+		return id_sprzedawcy;
+	}
+	
+	public List<Produkt> pobierzListeProduktow()
+	{
+		return produkty;
+	}
+	
+	//String data_dostawy w formacie dd/mm/yyyy
+	public void zamowienieZrealizowane(String data_dostawy)
+	{
+		status = new String("Zrealizowane");
+		SimpleDateFormat simple = new SimpleDateFormat("dd/MM/yyyy");
+		try 
+		{
+			this.data_dostawy = simple.parse(data_dostawy);
+		} 
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void dodajProdukt(Produkt produkt)
+	{
+		produkty.add(produkt);
+	}
+	
+	public void usunProdukt(Produkt produkt)
+	{
+		int tmp = produkty.indexOf(produkt);
+		produkty.remove(tmp);
+	}
+	
+	public String toString()
+	{
+		
+		return nr_zamowienia+","+status+","+czas_dostawy+","+data_zlozenia+","+data_dostawy+
+				","+id_sprzedawcy+","+produkty;
+	}
+
 }
