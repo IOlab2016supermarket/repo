c8bd7fe9aafdf4b12bd80960aeb2eb28bb293399
IOlab2016supermarket <iolab2016supermarket@gmail.com>
2016-12-07 15:02:15 +0800

    Dodanie modułu dostawy

---
+++ src/dostawa/Eksport.java
@@ -0,0 +1,18 @@
+package dostawa;
+import wspolne.Zamowienie;
+import java.util.List;
+
+
+public class Eksport {
+
+	public boolean zapiszZamowienia(List<Zamowienie> zamowienia)
+	{
+		return true;
+	}
+	
+	public boolean zapiszDostawcow(List<Dostawca> dostawcy)
+	{
+		return true;
+	}	
+	
+}
