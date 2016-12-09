4f8681986cbe7f77ac76dfb40f585b941f7b16f0
Magdalena Kowalska <kowalska.magdalena@op.pl>
2016-12-09 17:43:12 +0100

    Dodana tab dostawca i 2 pola w tab promocja.

--- Supermarket2016/src/baza_supermarket_MySQL.sql
+++ Supermarket2016/src/baza_supermarket_MySQL.sql
@@ -1,4 +1,4 @@
---CREATE DATABASE baza_supermarket;
+CREATE DATABASE baza_supermarket;
 --drop database baza_supermarket;
 USE baza_supermarket;
 
@@ -7,6 +7,14 @@ GRANT ALL PRIVILEGES ON *.* TO 'pracownik'@'%' IDENTIFIED BY 'Pracownik0.' WITH 
 
 CREATE USER 'pracownik'@'localhost' IDENTIFIED BY 'Pracownik0.';
 GRANT ALL PRIVILEGES ON *.* TO 'pracownik'@'localhost' IDENTIFIED BY 'Pracownik0.' WITH GRANT OPTION;
+
+
+CREATE TABLE dostawca 
+(
+     idDostawca INTEGER PRIMARY KEY NOT NULL , 
+     nazwa VARCHAR(50) ,
+	 adres VARCHAR(100)
+);
 
 CREATE TABLE logowanie 
 (
@@ -21,12 +29,9 @@ CREATE TABLE promocja
      doKiedy DATETIME , 
 	 idProduktu INTEGER ,
 	 staraCena INTEGER ,
-	 nowaCena INTEGER
-/*
-     id_partii INTEGER NOT NULL,
-     CONSTRAINT Promocje_data_rozp_wczesniejsza_niz_data_zak
-		CHECK (data_rozpoczecia < data_zakonczenia)
-*/
+	 nowaCena INTEGER ,
+	 cenaPoprzednia FLOAT ,
+	 cenaPromocyjna FLOAT
 );
 
 CREATE TABLE produkt
@@ -48,25 +53,6 @@ CREATE TABLE produkt
 	 CONSTRAINT promocja_produkt_FK 
 		FOREIGN KEY (idPromocji) REFERENCES promocja(idPromocji) 
 );
-/*
-CREATE TABLE magazyn 
-(
-     id INTEGER PRIMARY KEY NOT NULL , 
-     id_partii INTEGER NOT NULL , 
-     ustawienie_regalu CHAR(7) NOT NULL,
-     ilosc_produktow_w_regale INTEGER,
-	 CONSTRAINT Magazyn_Ustawienie_regalu_Pattern
-		CHECK (ustawienie_regalu LIKE 'URM[0-9][0-9][0-9][0-9]')
-);
-
-CREATE TABLE stanowiska 
-(
-     id INTEGER PRIMARY KEY NOT NULL , 
-     nazwa VARCHAR(30) , 
-     pensja FLOAT NOT NULL, 
-     budzet_operacyjny FLOAT
-);
-*/
 
 CREATE TABLE pracownik 
 (
@@ -76,28 +62,6 @@ CREATE TABLE pracownik
 	dataZatrudnienia DATETIME NOT NULL ,
 	nazwaStanowiska VARCHAR(30) NOT NULL ,
 	wynagrodzenie FLOAT
-/*
-     PESEL CHAR(11) PRIMARY KEY NOT NULL , 
-     id_stanowiska INTEGER , 
-     id_konta VARCHAR(20) NOT NULL, 
-     imie VARCHAR(30) NOT NULL, 
-     nazwisko VARCHAR(30) NOT NULL, 
-     premia FLOAT DEFAULT 0.0, 
-     data_zatrudnienia DATETIME NOT NULL, 
-     data_zwolnienia DATETIME , 
-     miasto VARCHAR(30) , 
-     ulica VARCHAR(40) , 
-     nr_budynku VARCHAR(5) , 
-     nr_mieszkania VARCHAR(5) , 
-     kod_pocztowy VARCHAR(6) , 
-     poczta VARCHAR(30),
-     CONSTRAINT Pracownicy_PESEL_Pattern
-		CHECK(PESEL LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-	 CONSTRAINT Pracownik_Stanowisko_FK 
-		FOREIGN KEY (id_stanowiska) REFERENCES stanowiska(id) ON DELETE SET NULL,
-     CONSTRAINT Pracownik_konta_FK 
-		FOREIGN KEY (id_konta) REFERENCES logowanie(login)
-*/
 );
 
 CREATE TABLE klient 
@@ -118,12 +82,6 @@ CREATE TABLE reklamacja
 		FOREIGN KEY (idProdukt) REFERENCES produkt(idProdukt) ,
 	 CONSTRAINT klient_reklamacja_FK 
 		FOREIGN KEY (idKlient) REFERENCES klient(idKlient)
-/*
-     data_zgloszenia DATETIME , 
-     opis VARCHAR(300),
-	CONSTRAINT Reklamacje_Produkty_FK 
-		FOREIGN KEY (id_produktu) REFERENCES produkt(idProdukt) 
-*/
 );
 
 CREATE TABLE faktura
@@ -157,15 +115,5 @@ CREATE TABLE zamowienie
 		FOREIGN KEY(idFaktura) REFERENCES faktura(idFaktura) ,
 	 CONSTRAINT klient_zam_FK 
 		FOREIGN KEY(idKlient) REFERENCES klient(idKlient)
-/*
-     nr_paragonu INTEGER NOT NULL, 
-     ilosc INTEGER , 
-     data_sprzedazy DATETIME , 
-     rodzaj_platnosci CHAR,
-     CONSTRAINT Transakcje_Pracownik_FK 
-		FOREIGN KEY (id_kasjera) REFERENCES pracownik(id),
-	 CONSTRAINT transakcje_Produkty_FK 
-		FOREIGN KEY(id_produktu) REFERENCES produkt(idProdukt) 
-*/
 );
 
